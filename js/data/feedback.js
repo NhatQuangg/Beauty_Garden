dffd45b7a0c93e3a3427f31bc713fd536ee06e9c
nhatquang2261@gmail.com <nhatquangg2261@gmail.com>
2023-07-29 15:36:46 +0700

    th

--- js/data/feedback.js
+++ js/data/feedback.js
@@ -2,54 +2,54 @@
 export default [
     {
         id: 1,
-        img: "../img/Ý kiến khách hàng/16112702_10208156288561212_4378727832274056704_o.jpg",
+        img: "./img/Ý kiến khách hàng/16112702_10208156288561212_4378727832274056704_o.jpg",
         fback: "Giá cả cực dễ chịu trên thị trường mỹ phẩm hiện nay. Chị nhân viên hôm qua tư vấn cho mình rất kỹ, nên nghe tư vấn xong mua muốn lủng cả túi. Sẽ là khách hàng trung thành của Beauty Garden.",
         name: "Hoàng Nhung",
 
     },
     {
         id: 2,
-        img: "../img/Ý kiến khách hàng/10406510_213960282128214_4383007999217281369_n.jpg",
+        img: "./img/Ý kiến khách hàng/10406510_213960282128214_4383007999217281369_n.jpg",
         fback: "Hôm nay đến shop mua son cho bạn gái mình cảm thấy rất hài lòng về bạn nhân viên bán hàng , bạn ấy đã giúp mình mua đúng màu son bạn mình thích. Shop đa dạng hàng hoá , trang trí đẹp và giá rẻ sẽ bảo bạn gái mình qua mua hàng thường xuyên. Mong shop luôn luôn có những bạn nhân viên tư vấn như bạn nữ ấy!",
         name: "EL Ninô",
     },
     {
         id: 3,
-        img: "../img/Ý kiến khách hàng/18221904_274323839695722_4187200371973568146_n.jpg",
+        img: "./img/Ý kiến khách hàng/18221904_274323839695722_4187200371973568146_n.jpg",
         fback: "Hai lần em đến shop mua tẩy da chết ST.IVES và sữa rửa mặt. Em đều rất thích mấy chị tư vấn rất nhiệt tình. Đặc biệt mấy chị luôn tươi cười kể cả chị tư vấn và chị thu ngân. Em sẽ luôn ủng hộ shop.",   
         name: "Minh Anh",
         },
     {
         id: 4,
-        img: "../img/Ý kiến khách hàng/17798924_1054472354697871_498115013455581690_n.jpg",
+        img: "./img/Ý kiến khách hàng/17798924_1054472354697871_498115013455581690_n.jpg",
         fback: "Hôm nay đến mua SP của shop rất chi là thích chị nhân viên đó, do da mình bị khô nên chị tư vấn rất kỹ lưỡng lại hay cười nữa , chị nhiệt tình ghê cơ , sp của shop e về dùng thích lắm luôn ạ . Cảm ơn chị nhiều nhé , lần sau sẽ qua và bảo chị tư vấn ak . Chị hay cười.",
         name: "Linh Đậu",
 
     },
     {
         id: 5,
-        img: "../img/Ý kiến khách hàng/17991991_636655289857415_2024600740020971408_n.jpg",
+        img: "./img/Ý kiến khách hàng/17991991_636655289857415_2024600740020971408_n.jpg",
         fback: "Yêu Shop. mãi ủng hộ shop. Đồ đa dạng, gần như là cái gì mình muốn mua shop đều có hết, mà kể cả mình chưa cần mua gì thì đến shop kiểu gì cũng phải khuân 1 cái gì đó về vì hàng lúc nào cũng tuyệt vời, giá thì yêu ko thể tả.",
         name: "Thái Phương Thảo",
 
     },
     {
         id: 6,
-        img: "../img/Ý kiến khách hàng/17264338_119423171922471_2018974082884941836_n.jpg",
+        img: "./img/Ý kiến khách hàng/17264338_119423171922471_2018974082884941836_n.jpg",
         fback: "Đã mua và cực kỳ hài lòng. Ở Đà Nẵng muốn tìm một nơi có rất nhiều mỹ phẩm làm đẹp như này thật sự là rất khó.",
         name: "Tiêu Dương",
         
     },
     {
         id: 7,
-        img: "../img/Ý kiến khách hàng/12651218_945171965579673_4150068264542304142_n.jpg",
+        img: "./img/Ý kiến khách hàng/12651218_945171965579673_4150068264542304142_n.jpg",
         fback: "Sản phẩm chính hãng nhiều kinh khủng, tới mua cho vợ món quà sinh nhật mà không biết chọn loại gì may mà nhờ mấy em tư vấn nhiệt tình. Cảm ơn Beauty Garden.",
         name: "Nguyễn Hiện",
         
     },
     {
         id: 8,
-        img: "../img/Ý kiến khách hàng/16602746_659432207573110_1651129273927689767_n-compressor.jpg",
+        img: "./img/Ý kiến khách hàng/16602746_659432207573110_1651129273927689767_n-compressor.jpg",
         fback: "Thích mua đồ ở shop vì nhân viên siêu cute, hôm trước mình ghé chi nhánh Phú Nhuận được chị nhân viên tư vấn nhiệt tình và chỉ thêm nhiều bí quyết dưỡng da . Yêu Shop...",
         name: "Nguyễn Thị Tuyết Trinh",
 
--- js/data/highlights.js
+++ js/data/highlights.js
@@ -2,68 +2,68 @@
 export default [
     {
         id: 1,
-        img: "../img/Thông tin nổi bật/160337178_4000984966590598_8347287454868613088_o.jpg",
+        img: "./img/Thông tin nổi bật/160337178_4000984966590598_8347287454868613088_o.jpg",
         text: 'Khách Hàng Beauty Garden quyên góp gần 100 triệu ủng hộ mua vắc xin COVID-19',
         infor: 'Sáng ngày 18/3/2021, đại diện Beauty Garden đã trao số tiền gần 100 triệu đồng (chính xác 92.800.000vnd) ủng hộ vào quỹ “Cùng Tuổi Trẻ góp vắc xin COVID-19”. Đây được xem là hành động thiết thực nhằm chung tay cùng toàn xã hội vượt qua đại dịch.',        
     },
     {
         id: 2,
-        img: "../img/Thông tin nổi bật/anhr-hihihi-19062019113819.jpg",
+        img: "./img/Thông tin nổi bật/anhr-hihihi-19062019113819.jpg",
         text: 'ĐĂNG KÍ BEAUTY GARDEN ZALO OFFICAL ACCOUNT - NHẬN NGAY QUYỀN LỢI ĐẶC BIỆT',
         infor: 'Sau gần 5 năm hoạt động, Beauty Garden đã có những bước tiến dài rộng với 6 cửa hàng trên khắp 3 miền đất nước. Với mong muốn không chỉ chăm sóc khách hàng tốt trên "mặt trận" Fanpage và Website, BeautyGarden còn mong muốn được hỗ trợ khách hàng ở mọi lúc mọi nơi, giúp khách hàng dễ dàng kết nối, chia sẻ, tìm kiếm thông tin một cách tiện lợi và nhanh chóng.',        
     },
     {
         id: 3,
-        img: "../img/Thông tin nổi bật/2M8A9937 (2).jpg",
+        img: "./img/Thông tin nổi bật/2M8A9937 (2).jpg",
         text: "Review đánh giá mặt nạ giấy Innisfree It's Real Squeeze Mask",
         infor: "Beauty Garden sẽ review mặt nạ giấy Innisfree It's Real Squeeze Mask một dòng mặt giấy chiết xuất từ thiên nhiên an toàn và nhẹ nhàng cho làn da của bạn.",
         
     },
     {
         id: 4,
-        img: "../img/Thông tin nổi bật/web-min(1).jpg",
+        img: "./img/Thông tin nổi bật/web-min(1).jpg",
         text: "GÓC TUYỂN DỤNG - BEAUTY GARDEN TÌM NGƯỜI THÂN",
         infor: 'Đại gia đình Beauty Garden “phất cờ” tìm người thân!! "Người thương" nào đang tìm kiếm một môi trường làm việc trẻ trung, năng động, có nhiều cơ hội thăng tiến trong tương lai thì đừng ngần ngại gửi hồ sơ cho chúng mình nhé! Beauty Garden TP. Hồ Chí Minh cần bạn!',
         
     },
     {
         id: 5,
-        img: "../img/Thông tin nổi bật/dautaytrangmuji.jpg",
+        img: "./img/Thông tin nổi bật/dautaytrangmuji.jpg",
         text: "Review dầu tẩy trang Muji Mild Cleansing Oil Nhật Bản",
         infor: "Dầu tẩy trang là loại mỹ phẩm làm đẹp không thể thiếu của mỗi cô nàng, nhất là những cô nàng mê makeup. Hôm nay, Beauty Garden sẽ giới thiệu thêm cho các nàng một dòng tẩy trang cực chất lượng đến từ Nhật Bản, đang được phái đẹp cả thế giới săn lùng. Cùng tham khảo bài review dầu tẩy trang Muji Mild Cleansing Oil.",
         
     },
     {
         id: 6,
-        img: "../img/Thông tin nổi bật/3_3.jpg",
+        img: "./img/Thông tin nổi bật/3_3.jpg",
         text: "Review các dòng serum của Timeless",
         infor: "Timeless là thương hiệu mà cách đây khoảng 2 năm tại Việt Nam chưa ai biết đến, nhưng thời gian gần đây nó lại là một cái tên được tìm kiếm nhiều nhất trên các trang mạng tìm kiếm về từ khóa “Serum”. Vì sao serum Timless lại được yêu thích đến như vậy? Hãy theo chân Beauty Garden tìm hiểu nhé!",
         
     },
     {
         id: 7,
-        img: "../img/Thông tin nổi bật/4_5.jpg",
+        img: "./img/Thông tin nổi bật/4_5.jpg",
         text: "Review April Skin Magic Snow Cushion cực hot",
         infor: "Đây là loại phấn nước được cho là phấn nước “ma thuật” hay phấn nước “phù thủy”,…. Có rất nhiều biệt danh được “phủ” lên nó, và tại sao lại như vậy? Mình đã tìm được nó trong một lần đến cửa hàng mỹ phẩm chính hãng Beauty Garden để “truy tìm” phấn nước hợp với da mình. Hãy cùng mình khám phá xem bên trong sản phẩm này như thế nào nha.",
         
     },
     {
         id: 8,
-        img: "../img/Thông tin nổi bật/2M8A8716.jpg",
+        img: "./img/Thông tin nổi bật/2M8A8716.jpg",
         text: "Review nước hoa hồng Evoluderm Lotion Tonique chất lượng cho mọi loại da",
         infor: "Sử dụng nước hoa hồng để chăm sóc da từ lâu đã trở thành thói quen của nhiều chị em chúng mình. Vậy trong số các dòng nước hoa hồng bạn từng dùng, bạn đã biết đến dòng nước hoa hồng Evoluderm chưa nhỉ? Một dòng nước hoa hồng có giá bình dân nhưng chất lượng thì hẳn sẽ khiến nhiều người bất ngờ.",
         
     },
     {
         id: 9,
-        img: "../img/Thông tin nổi bật/1472453808_20160824_194734-1.jpg",
+        img: "./img/Thông tin nổi bật/1472453808_20160824_194734-1.jpg",
         text: "Review nước hoa hồng không cồn Thayers Alcohol Free Witch Hazel Toner",
         infor: "Nàng nào đang cần một sản phẩm nước hoa hồng mà ngại cồn thì hãy cùng Beauty Garden xem review nước hoa hồng Thayers Alcohol Free Witch Hazel Toner để chăm sóc da khỏe và đẹp hơn nhé! Chắc chắn các nàng sẽ phải bất ngờ trước dòng nước hoa hồng đang được rất nhiều cô gái yêu thích hiện nay.",
         
     },
     {
         id: 10,
-        img: "../img/Thông tin nổi bật/kemnenmabelline1.jpg",
+        img: "./img/Thông tin nổi bật/kemnenmabelline1.jpg",
         text: "Review kem nền thần thánh Maybelline Fit Me",
         infor: "Kem nền là món mỹ phẩm makeup không thể thiếu của mỗi cô nàng. Nhất là với những nàng mê makeup. Dù có dùng thử qua biết bao nhiêu là Cushion hay BB Cream, CC Cream thì cuối cùng các chị em đều có chung kết luận rằng “Kem nền mới là chân ái”. Hôm nay Beauty Garden sẽ giới thiệu cho các bạn 1 sản phẩm kem nền bình dân của hãng mỹ phẩm Maybelline – Maybelline Fit Me. Hãy cùng tìm hiểu xem sản phẩm này có thần thánh như lời đồn không nhé.",
     },
